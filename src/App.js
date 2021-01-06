2abc54fc8e817a768f0b1f8210ae4a2ccd47d9bd
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2021-01-06 14:11:28 -0500

    scaffolding done. next: api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import { CssBaseline } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <CssBaseline />
+      <Switch>
+        <Route>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
